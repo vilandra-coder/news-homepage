847559e06a899abaa062f2349caa6c7d1ecb5305
vilandra-coder <ohaerifaith02@gmail.com>
2022-11-08 11:16:30 +0100

    final commit

---
+++ js/game.js
@@ -0,0 +1,16 @@
+function navi() {
+  let x = document.getElementById("menuEl");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function dis() {
+  let y = document.getElementById("menuEl");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "block";
+  }
+}
